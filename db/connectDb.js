5120391126b54ed2b37c9e6c633728baf72d9dd7
Batuhan <batuhan_.ay@hotmail.com>
2022-07-01 00:00:42 +0300

    Added db creation for unknown db

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const mysql = require('mysql2/promise');
 
 const sequelize = new Sequelize(
     process.env.DATABASE_DB,
@@ -20,7 +21,20 @@ sequelize
         console.log('Connection has been established successfully.');
     })
     .catch(err => {
+
+        if(err.message.includes('Unknown database')) {
+
+            return mysql.createConnection({ 
+                host: process.env.DATABASE_HOST,
+                port: process.env.DATABASE_PORT,
+                user: process.env.DATABASE_USER,
+                password: process.env.DATABASE_PASSWORD 
+            })
+            .then(connection => connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DATABASE_DB}\`;`))
+
+        }
         console.log('Unable to connect to the database:', err);
+
     });
 
 // Export
